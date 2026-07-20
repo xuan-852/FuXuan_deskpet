8ae18bd9fa78fa4af8ca6c1ddf31eaa66182883b
xuan <925110800246@njust.edu.cn>
2026-07-20 13:23:22 +0800

    🧹 清理临时构建日志、诊断报告、分析脚本等无用文件

--- build_temp2.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-subst Z: /d >nul 2>&1
-subst Z: D:\Unity\projects\Desktop_per_pro
-Z:
-cd Z:\
-"D:\Unity\editor\2022.3.62t7\Editor\Tuanjie.exe" -batchmode -quit -projectPath Z:\ -logFile build_log13.txt -executeMethod BuildScript.BuildDesktopPet
